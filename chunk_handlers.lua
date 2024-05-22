09d8fcf5d63f08e746f86c0de62e6e0a0eb5a5c0
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-05-22 14:22:36 +1200

    chunks? i handle them.

--- chunk_handlers.lua
+++ chunk_handlers.lua
@@ -2,7 +2,7 @@
 ---@field type string
 ---@field data table<string, any>
 
----@alias handler_fn fun(reader: byte_reader, length: integer, chunk_handlers: handler_result[]): handler_result
+---@alias handler_fn fun(reader: byte_reader, length: integer, chunk_handlers: handler_result[]): table<string, any>
 
 ---@param chunk_handlers handler_result[]
 ---@param to_locate string
@@ -15,6 +15,12 @@ local function locate_chunk(chunk_handlers, to_locate)
 	end
 	error("could not find requested chunk " .. to_locate)
 end
+
+---@enum
+local PALLETE_TYPE = {
+	true_colour = 2,
+	indexed_colour = 3,
+}
 
 ---@type table<string, handler_fn>
 local M = {
@@ -49,6 +55,36 @@ local M = {
 		local IDHR = locate_chunk(chunk_handlers, "IHDR")
 		---@type integer
 		local colour_type = IDHR.data.colour_type
+		local data = {}
+		if colour_type == PALLETE_TYPE.indexed_colour then
+			data.pallete_transparency = {}
+			for i = 0, length - 1 do
+				data.pallete_transparency[i] = reader:read_be(1)
+			end
+		elseif colour_type == PALLETE_TYPE.true_colour then
+			data.transparent_colour = {
+				red = reader:read_be(2),
+				green = reader:read_be(2),
+				blue = reader:read_be(2),
+			}
+		else
+			error("Cannot include tRNS chunk in colour type " .. colour_type)
+		end
+		return data
+	end,
+	IDAT = function(reader, length, chunk_handlers)
+		local success = pcall(locate_chunk, chunk_handlers, "IDAT")
+		if success and not chunk_handlers[#chunk_handlers].type == "IDAT" then
+			error("Gap between IDAT chunks")
+		end
+		local bytes = {}
+		for i = 0, length - 1 do
+			bytes[i] = reader:read_be(1)
+		end
+		return { bytes = bytes }
+	end,
+	IEND = function(reader, length, chunk_handlers)
+		return {}
 	end,
 }
 
